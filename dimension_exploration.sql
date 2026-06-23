68617a31889ce06c2f615f685344ae4ce1d9220a
niku2712 <127451692+niku2712@users.noreply.github.com>
2026-06-23 16:20:30 +0530

    Delete dimension_exploration.sql

--- dimension_exploration.sql
+++
@@ -1,8 +0,0 @@
--- explore all countries our customers come from
-
-select distinct country from gold.dim_customers
-
--- explore all the categories "the major divisions"
-
-select distinct category, subcategory, product_name from gold.dim_products
-order by 1,2,3
