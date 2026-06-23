a18a173419e0317288706f81fc3d2ef823a403b6
niku2712 <127451692+niku2712@users.noreply.github.com>
2026-06-23 16:20:00 +0530

    Delete data_exploration.sql

--- data_exploration.sql
+++
@@ -1,8 +0,0 @@
--- explore all objects in the database
-
-select * from information_schema.tables
-
--- explore all the columns in the database
-
-select * from INFORMATION_SCHEMA.columns
-where table_name = 'dim_customers'
